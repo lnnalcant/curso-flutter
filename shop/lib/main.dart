abd0023b751d693ef5de436a0b5c7764b2325436
lnnalcant <lenonalcant@hotmail.com>
2021-02-17 13:17:48 -0300

    feito até aula 208

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shop/utils/app_routes.dart';
 import './views/products_overview_screen.dart';
+import './views/product_detail_screen.dart';
+import './utils/app_routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,6 +17,9 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Lato',
       ),
       home: ProductOverviewScreen(),
+      routes: {
+        AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
+      },
     );
   }
 }
--- shop/lib/views/product_detail_screen.dart
+++ shop/lib/views/product_detail_screen.dart
@@ -2,18 +2,14 @@ import 'package:flutter/material.dart';
 import '../models/product.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  final Product product;
-
-  ProductDetailScreen(this.product);
-
   @override
   Widget build(BuildContext context) {
+    final Product product =
+        ModalRoute.of(context).settings.arguments as Product;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
-      ),
-      body: Center(
-        child: Text('Detalhes do produto'),
       ),
     );
   }
